936b2d09305d87051c3d23efca66e4a857b7f085
LachezarIvanovv <lachezar.ivanov.0888@abv.bg>
2023-07-01 14:07:39 +0300

    Optimization

---
+++ src/main/java/softuni/pathfinder/config/InterceptorConfigurator.java
@@ -0,0 +1,15 @@
+package softuni.pathfinder.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import softuni.pathfinder.interceptors.LoggingInterceptor;
+
+@Configuration
+public class InterceptorConfigurator implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
+    }
+}
--- src/main/java/softuni/pathfinder/web/AuthController.java
+++ src/main/java/softuni/pathfinder/web/AuthController.java
@@ -2,6 +2,7 @@ package softuni.pathfinder.web;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -56,6 +57,17 @@ public class AuthController {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @PostMapping("/login-error")
+    public String onFailedLogin(
+            @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username,
+            RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, username);
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+
+        return "redirect:/login";
     }
 
 
